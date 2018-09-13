a6c18ea181d64f52331c8bd0077a6078fa314dfe
Adeniran Samuel <adeniransamuel93@gmail.com>
2018-09-13 21:14:28 +0100

    Set up server

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import Joi from 'joi';
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/', (req, res) => {
+	res.send('Fast Food App');
+});
+
+const port = 3000;
+app.listen(port, () => {
+	console.log(`Server started on PORT ${port}..`);
+});
+
+module.exports = app;
