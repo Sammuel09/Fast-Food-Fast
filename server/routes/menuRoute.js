8139495bb15a473f533642108ca87d4893765105
Adeniran Samuel <adeniransamuel93@gmail.com>
2018-09-28 17:45:40 +0100

    - Allows users to get all menu - this route is protected for only authenticated users

--- server/routes/menuRoute.js
+++ server/routes/menuRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import menuValidator from '../middlewares/menuValidator';
 import menuController from '../controllers/menuController';
-import jwtCheck from '../middlewares/jwtCheck';
-import Auth from '../middlewares/Auth';
+import auth from '../middlewares/auth';
 
 const router = express.Router();
 
-router.post('/menu', menuValidator.validateMenu, Auth.verifyAdmin, menuController.createMenu);
+router.post('/menu', menuValidator.validateMenu, auth.verifyAdmin, menuController.createMenu);
+
+router.get('/menu', auth.verifyAuth, menuController.getAllMenu);
 
 export default router;
