a8194a90e9eee6af5db08d7a37a538bc721d4c02
Adeniran Samuel <adeniransamuel93@gmail.com>
2018-11-09 19:13:35 +0100

    - submit image to cloudinary

--- public/javascript/postmenu.js
+++ public/javascript/postmenu.js
@@ -8,57 +8,25 @@ const CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/dx8yi1fsi/image/upload';
 const CLOUDINARY_UPLOAD_PRESET = 'ruamt9zr';
 
 
-// const handleResponse = response => response.json()
-//   .then((json) => {
-//     if (response.ok) {
-//       return json;
-//     } if (!response.ok) {
-//       throw new Error(JSON.stringify(json));
-//     }
-//   });
+const handleResponse = response => response.json()
+  .then((json) => {
+    if (response.ok) {
+      return json;
+    } if (!response.ok) {
+      throw new Error(JSON.stringify(json));
+    }
+  });
 
-imageurl.addEventListener('change', (event) => {
-  const file = event.target.files[0];
-  console.log (file);
-  const formData = new FormData();
-  formData.append('file', file);
-  formData.append('upload_preset', 'savri87we342');
-  fetch(CLOUDINARY_URL, {
-    method: 'POST',
-    mode: 'no-cors',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: formData,
-  })
-    .then(response => response.json())
-    .then((response) => {
-      console.log(response);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
-
-
-// const handleResponse = response => response.json()
-//   .then((json) => {
-//     if (response.ok) {
-//       return json;
-//     } if (!response.ok) {
-//       throw new Error(JSON.stringify(json));
-//     }
-//   });
-
-const sendFormData = () => {
+const sendFormData = (imgurl) => {
   fetch('https://sammie-fast-food-fast.herokuapp.com/api/v1/menu', {
     method: 'post',
     headers: {
       'content-type': 'application/json',
     },
     body: JSON.stringify({
-      email: document.querySelector('#name').value,
-      password: document.querySelector('#price').value,
+      name: document.querySelector('#name').value,
+      imageurl: imgurl,
+      price: document.querySelector('#price').value,
     }),
   })
     .then(handleResponse)
@@ -70,9 +38,53 @@ const sendFormData = () => {
     })
     .catch((error) => {
       console.log(error);
+      successMsg.innerHTML = error;
+      // errorMsg.innerHTML = (JSON.parse(error.message)).message;
     });
 };
+
 postMenuBtn.addEventListener('click', (event) => {
   event.preventDefault();
-  sendFormData();
+  const file = imageurl.files[0];
+  console.log(file);
+  const formData = new FormData();
+  formData.append('file', file);
+  formData.append('upload_preset', 'savri87we342');
+  fetch(CLOUDINARY_URL, {
+    method: 'POST',
+    body: formData,
+  })
+    .then(handleResponse)
+    .then((res) => {
+      const imgurl = res.secureurl;
+      console.log(res);
+      sendFormData(imgurl);
+    })
+    .catch((res) => {
+      console.log(res);
+    });
 });
+
+// imageurl.addEventListener('change', (event) => {
+//   const file = event.target.files[0];
+//   console.log (file);
+//   const formData = new FormData();
+//   formData.append('file', file);
+//   formData.append('upload_preset', 'savri87we342');
+//   fetch(CLOUDINARY_URL, {
+//     method: 'POST',
+//     body: formData,
+//   })
+//     .then(handleResponse)
+//     .then((res) => {
+//       console.log(JSON.stringify(res));
+//     })
+//     .catch((res) => {
+//       console.log(res);
+//     });
+// });
+
+// postMenuBtn.addEventListener('click', (event) => {
+//   event.preventDefault();
+//   sendFormData();
+// });
